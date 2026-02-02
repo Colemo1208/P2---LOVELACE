36ce99c9380eda7e54840aa33202becc362bb5fb
caua <cauaamorimalmeida@gmail.com>
2026-02-02 08:57:21 -0300

    Apagando pasta errada

--- projetos_2_engcommp_lovelace/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.projetos2engcommplovelace
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- projetos_2_engcommp_lovelace/lib/index.dart
+++
@@ -1,4 +0,0 @@
-// Export pages
-export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
-export '/pages/resultadospage/resultadospage_widget.dart'
-    show ResultadospageWidget;
