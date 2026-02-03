e7ce2de6135be85150aeb6a8dc530d0ecc1342a0
caua <cauaamorimalmeida@gmail.com>
2026-02-03 16:08:37 -0300

    atualizando fluttercode

---
+++ fluttercode/projetos_2_engcommp_lovelace/lib/pages/home_page/home_page_model.dart
@@ -0,0 +1,68 @@
+import '/backend/api_requests/api_calls.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'home_page_widget.dart' show HomePageWidget;
+import 'package:flutter/material.dart';
+
+class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  ///  Local state fields for this page.
+
+  String? objetivoSelecionado = 'null';
+
+  List<dynamic> listacelulares = [];
+  void addToListacelulares(dynamic item) => listacelulares.add(item);
+  void removeFromListacelulares(dynamic item) => listacelulares.remove(item);
+  void removeAtIndexFromListacelulares(int index) =>
+      listacelulares.removeAt(index);
+  void insertAtIndexInListacelulares(int index, dynamic item) =>
+      listacelulares.insert(index, item);
+  void updateListacelularesAtIndex(int index, Function(dynamic) updateFn) =>
+      listacelulares[index] = updateFn(listacelulares[index]);
+
+  String usoselecionado = 'geral\n';
+
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for PageView widget.
+  PageController? pageViewController;
+
+  int get pageViewCurrentIndex =>
+      pageViewController != null &&
+          pageViewController!.hasClients &&
+          pageViewController!.page != null
+      ? pageViewController!.page!.round()
+      : 0;
+  // State field(s) for Slider widget.
+  double? sliderValue;
+  // State field(s) for ChoiceChips widget.
+  FormFieldController<List<String>>? choiceChipsValueController1;
+  List<String>? get choiceChipsValues1 => choiceChipsValueController1?.value;
+  set choiceChipsValues1(List<String>? val) =>
+      choiceChipsValueController1?.value = val;
+  // State field(s) for ChoiceChips widget.
+  FormFieldController<List<String>>? choiceChipsValueController2;
+  String? get choiceChipsValue2 =>
+      choiceChipsValueController2?.value?.firstOrNull;
+  set choiceChipsValue2(String? val) =>
+      choiceChipsValueController2?.value = val != null ? [val] : [];
+  // State field(s) for ChoiceChips widget.
+  FormFieldController<List<String>>? choiceChipsValueController3;
+  String? get choiceChipsValue3 =>
+      choiceChipsValueController3?.value?.firstOrNull;
+  set choiceChipsValue3(String? val) =>
+      choiceChipsValueController3?.value = val != null ? [val] : [];
+  // State field(s) for ChoiceChips widget.
+  FormFieldController<List<String>>? choiceChipsValueController4;
+  String? get choiceChipsValue4 =>
+      choiceChipsValueController4?.value?.firstOrNull;
+  set choiceChipsValue4(String? val) =>
+      choiceChipsValueController4?.value = val != null ? [val] : [];
+  // Stores action output result for [Backend Call - API (Buscarcelulares)] action in Button widget.
+  ApiCallResponse? resultadofinal;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
